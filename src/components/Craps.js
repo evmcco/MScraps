e7eb8309bc221d821a9ca94ec36bfdf658cfaf6c
Evan McCoy <evan.mccoy@gmail.com>
2019-07-15 09:47:16 -0400

    dice rolling to console

--- src/components/Craps.js
+++ src/components/Craps.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
 
 class Craps extends Component {
+  rollDice = () => {
+    const die1 = Math.floor(Math.random() * 6 + 1);
+    const die2 = Math.floor(Math.random() * 6 + 1);
+    console.log("Die 1:", die1, "Die 2", die2);
+    return [die1, die2];
+  };
+
   render() {
     return (
       <>
