dd929c490670f88d147be7af2f1f0e85beab1317
Evan McCoy <evan.mccoy@gmail.com>
2019-07-15 13:29:54 -0400

    implemented generic handleChange

---
+++ src/components/Board.js
@@ -0,0 +1,73 @@
+import React, { Component } from "react";
+
+class Board extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // bets: {
+      //   pass: 0,
+      //   dontPass: 0,
+      //   come: 0,
+      //   dontCome: 0,
+      //   place4: 0,
+      //   place5: 0,
+      //   place6: 0,
+      //   place8: 0,
+      //   place9: 0,
+      //   place10: 0,
+      //   field: 0
+      // }
+      bets: this.props.bets
+      // bets: {}
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handlePassChange = e => {
+    this.setState({
+      bets: { ...this.state.bets, pass: e.target.value }
+    });
+  };
+
+  handleChange = e => {
+    const newBets = { ...this.state.bets };
+    const target = e.target;
+    const value = target.value;
+    const name = target.name;
+    newBets[name] = value;
+    this.setState({
+      bets: newBets
+    });
+  };
+
+  render() {
+    return (
+      <>
+        <label>
+          Pass
+          <input
+            type="text"
+            name="pass"
+            onChange={this.handleChange}
+            value={this.state.bets.pass}
+          />
+        </label>
+        <label>
+          Don't Pass
+          <input
+            type="text"
+            name="dontPass"
+            onChange={this.handleChange}
+            value={this.state.bets.dontPass}
+          />
+        </label>
+        <button onClick={() => this.props.acceptBets(this.state.bets)}>
+          Place Bets
+        </button>
+      </>
+    );
+  }
+}
+
+export default Board;
--- src/components/Craps.js
+++ src/components/Craps.js
@@ -1,17 +1,38 @@
 import React, { Component } from "react";
+import Board from "./Board";
+import { throwStatement } from "@babel/types";
 
 class Craps extends Component {
   state = {
     die1: 0,
-    die2: 0
+    die2: 0,
+    point: 0,
+    pointSet: false,
+    allRolls: [],
+    bets: {
+      pass: 0,
+      dontPass: 0,
+      come: 0,
+      dontCome: 0,
+      place4: 0,
+      place5: 0,
+      place6: 0,
+      place8: 0,
+      place9: 0,
+      place10: 0,
+      field: 0
+    }
   };
 
   rollDice = () => {
     const die1 = Math.floor(Math.random() * 6 + 1);
     const die2 = Math.floor(Math.random() * 6 + 1);
+    let newAllRolls = this.state.allRolls.slice();
+    newAllRolls.push([die1, die2]);
     this.setState({
       die1: die1,
-      die2: die2
+      die2: die2,
+      allRolls: newAllRolls
     });
   };
 
@@ -31,6 +52,46 @@ class Craps extends Component {
     ) : null;
   };
 
+  acceptBets = bets => {
+    this.setState({
+      bets
+    });
+  };
+
+  getResults = () => {
+    const diceSum = this.state.die1 + this.state.die2;
+    if (!this.state.pointSet) {
+      if (diceSum === 2 || diceSum === 3 || diceSum === 12) {
+        //pass loses, dont pass wins
+      } else if (diceSum === 7 || diceSum === 11) {
+        //pass wins, dont pass loses
+      } else if (
+        diceSum === 4 ||
+        diceSum === 5 ||
+        diceSum === 6 ||
+        diceSum === 8 ||
+        diceSum === 9 ||
+        diceSum === 10
+      ) {
+        this.setState({
+          point: diceSum,
+          pointSet: true
+        });
+      }
+    } else if (!!this.state.pointSet) {
+      if (diceSum === 7) {
+        //dont pass wins, clear bets and reset point
+        this.setState({
+          pointSet: false
+        });
+      } else if (diceSum === this.state.point) {
+        //pass wins
+      } else {
+        //keep rolling
+      }
+    }
+  };
+
   render() {
     console.log(this.state);
     return (
@@ -44,6 +105,10 @@ class Craps extends Component {
             <span>{this.dieNumberToIcon(this.state.die2)}</span>
           ) : null}
         </h1>
+        <Board
+          acceptBets={bets => this.acceptBets(bets)}
+          bets={this.state.bets}
+        />
       </>
     );
   }
